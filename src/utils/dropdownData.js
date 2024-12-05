46ef4b8c25678affbbec79caa5d1a1effdc06927
vdudeja-coforge <vishal.dudeja@coforge.com>
2024-12-05 18:48:03 +0530

    Updates

--- src/utils/dropdownData.js
+++
@@ -1,5 +0,0 @@
-export const airlinesOptions =  [ "BRITISH AIRWAYS" , "EASYJET UK LTD" , "WIZZ AIR UK LTD" , "TUI AIRWAYS LTD" , "VIRGIN ATLANTIC AIRWAYS LTD" ];
-
-export const reviewPlatformsOptions =  [ "Trustpilot" , "GOOGLE REVIEWS" , "Tripadvisor" , "FACEBOOK" , "HomeViews" ];
-
-export const viewOptions =  [ "Table" , "Chart" ];
