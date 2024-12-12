7a9a59596591c7fe1ed0e8d528d5beb1850d2ec8
vdudeja-coforge <vishal.dudeja@coforge.com>
2024-12-12 13:27:54 +0530

    chart ui center

--- src/components/AirlineComparisonChartView.jsx
+++ src/components/AirlineComparisonChartView.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const AirlineComparisonChartView = () => {
     // Component logic and JSX here
     return (
-            <div style={{display: "flex", justifyContent:"space-around", flexDirection:"column", paddingTop:"40px" }}>
+            <div style={{display: "flex", justifyContent:"center", alignItems:"center", paddingTop:"60px",  }}>
                 <h2>Chart View Coming Soon...</h2>
             </div> 
     );
